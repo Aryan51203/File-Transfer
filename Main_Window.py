4e31d7ab3293c6e2b147d492efe4cf28f2225fb5
Aryan51203 <krish7664@gmail.com>
2023-09-09 07:01:07 +0530

    Added UI elements for creating server

--- Main_Window.py
+++ Main_Window.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import filedialog
 
 import customtkinter as ctk
 from PIL import Image, ImageTk
@@ -7,6 +8,15 @@ from Client import connectToServer
 
 import createServer
 import joinServer
+
+def selectFolder(inp):
+    root = Tk()
+    root.attributes("-topmost", True)  # Display the dialog in the foreground.
+    root.iconify()  # Hide the little window.
+    fp = filedialog.askdirectory()
+    root.destroy()  # Destroy the root window when folder selected.
+    
+    inp.set(fp)
 
 
 def main_frame():
@@ -42,8 +52,8 @@ def main_frame():
     )
     btn2.place(x=550, y=200)
 
-    # btn3 = ctk.CTkButton(master=frame, text = "H0ST A FILE OR FOLDER",font=('Comic Sans MS',22),command=lambda: Host_Frame(frame))
-    # btn3.place(x=550,y=250)
+    btn3 = ctk.CTkButton(master=frame, text = "H0ST A FILE OR FOLDER",font=('Comic Sans MS',22),command=lambda: Host_Frame(frame))
+    btn3.place(x=650,y=250)
 
     btn4 = ctk.CTkButton(
         master=frame, text="EXIT", font=("Comic Sans MS", 22), command=window.destroy
@@ -76,11 +86,56 @@ def Join_Frame(main_frame_delete):
             joinServer.joinServer(frame1, main_frame, inpIP, sock)
 
 
-# def Host_Frame(main_frame_delete):
-#     main_frame_delete.destroy()
-#     frame1 = ctk.CTkFrame(master=window)
-#     frame1.pack(fill="both", expand=1)
-#     createHost.createHost(frame1, main_frame)
+def Host_Frame(main_frame_delete):
+    root=ctk.CTkToplevel(window)
+    root.geometry("400x400")
+    root.title("Host Server")
+
+    lb1 = ctk.CTkLabel(
+        root,
+        text="Create Server",
+        font=("Comic Sans MS bold", 34),
+        padx=5,
+        pady=5,
+    )
+    lb1.pack()
+
+    inputGot = StringVar()
+    inp1=ctk.CTkEntry(master=root,textvariable=inputGot)
+    inp1.pack()
+
+    btn1 = ctk.CTkButton(
+        master=root,
+        text="BROWSE",
+        font=("Comic Sans MS", 22),
+        command=lambda:selectFolder(inputGot),
+        # compound=BOTTOM,
+    )
+    btn1.pack()
+
+    btn2 = ctk.CTkButton(
+        master=root,
+        text="CREATE",
+        font=("Comic Sans MS", 22),
+        # compound=BOTTOM,
+    )
+    btn2.pack()
+
+    btn3 = ctk.CTkButton(
+        master=root,
+        text="CANCEL",
+        font=("Comic Sans MS", 22),
+        command=root.destroy,
+        # compound=BOTTOM,
+    )
+    btn3.pack()
+
+    root.mainloop()
+
+    # main_frame_delete.destroy()
+    # frame1 = ctk.CTkFrame(master=window)
+    # frame1.pack(fill="both", expand=1)
+    # createHost.createHost(frame1, main_frame)
 
 
 window = ctk.CTk()
@@ -92,7 +147,7 @@ join_image = ImageTk.PhotoImage(
 )
 
 window.title("File Transfer")
-window.resizable()
+window.resizable(0,0)
 # window.state('zoomed')
 window.geometry("1000x600+700+200")
 
